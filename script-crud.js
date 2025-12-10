c136ba65d4183b538f7511aeeb5452faf11115d8
LuketeS <lsschmalz@gmail.com>
2025-12-10 15:21:02 -0300

    feat: armazena a descrição da tarefa adicionado no LocalStorage

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,18 @@
 const btnAddTask = document.querySelector(".app__button--add-task");
 const formAddTask = document.querySelector(".app__form-add-task");
+const formTextArea = document.querySelector(".app__form-textarea");
+
+const tasks = []
 
 btnAddTask.addEventListener("click", () => {
     formAddTask.classList.toggle("hidden");
+})
+
+formAddTask.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    const task = {
+        descricao: formTextArea.value
+    }
+    tasks.push(task);
+    localStorage.setItem("tarefas",JSON.stringify(tasks));
 })
